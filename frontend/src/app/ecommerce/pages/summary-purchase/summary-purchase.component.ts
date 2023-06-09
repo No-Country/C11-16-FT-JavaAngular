12b08682043a50d1ec92f9b7150a7d7800899d92
Metaldev-06 <fernydiaz62@gmail.com>
2023-06-08 22:51:20 -0300

    Add function summary

--- frontend/src/app/ecommerce/pages/summary-purchase/summary-purchase.component.ts
+++ frontend/src/app/ecommerce/pages/summary-purchase/summary-purchase.component.ts
@@ -4,6 +4,14 @@ import { ActivatedRoute, Router } from '@angular/router';
 import { Trip } from 'src/app/interfaces/trip_interface';
 import { DataService } from 'src/app/services/data.service';
 
+import { Loading, Notify } from 'notiflix';
+
+import * as pdfMake from 'pdfmake/build/pdfmake';
+import * as pdfFonts from 'pdfmake/build/vfs_fonts';
+import { HttpClient } from '@angular/common/http';
+
+(<any>pdfMake).vfs = pdfFonts.pdfMake.vfs;
+
 interface Popular {
   id: number;
   title: string;
@@ -24,6 +32,7 @@ export class SummaryPurchaseComponent implements OnInit {
   route = inject(ActivatedRoute);
   formBuilder = inject(FormBuilder);
   router = inject(Router);
+  http = inject(HttpClient);
 
   ngOnInit(): void {
     window.scroll(0, 1009);
@@ -62,4 +71,106 @@ export class SummaryPurchaseComponent implements OnInit {
       img: '../../../../../../assets/images/montaña-min-dos.png',
     },
   ];
+
+  createPdf() {
+    const user = JSON.parse(localStorage.getItem('userData')!);
+
+    const pdfDefinitio: any = {
+      content: [
+        {
+          text: 'Tango Viajes',
+          style: 'nombreEmpresa',
+        },
+        {
+          text: 'Resumen de compra',
+          style: 'resumen',
+        },
+        {
+          text: `Gracias por tu compra ${user.name} ${user.lastname}`,
+          style: 'header',
+        },
+        {
+          text: `Hotel: ${this.trip.hotel.title}`,
+        },
+        { text: `Descripción: ${this.trip.hotel.description}` },
+        {
+          text: `Abonaste en tu tarjeta Visa Finalizada en ${this.cardNumber}`,
+        },
+        { text: `Precio: ${this.trip.price}` },
+        {
+          text: `Total impuestos: $${this.trip.price * 0.21}`,
+          style: 'anotherStyle',
+        },
+        {
+          text: `Total abonados: $${this.trip.price * 1.21}`,
+          style: 'anotherStyle',
+        },
+      ],
+
+      styles: {
+        resumen: {
+          fontSize: 25,
+          lineHeight: 4,
+          alignment: 'center',
+          bold: true,
+        },
+        nombreEmpresa: {
+          fontSize: 30,
+          lineHeight: 4,
+          alignment: 'right',
+          bold: true,
+        },
+        header: {
+          fontSize: 20,
+          bold: true,
+          lineHeight: 3,
+        },
+        anotherStyle: {
+          italics: true,
+          alignment: 'right',
+          bold: true,
+        },
+      },
+    };
+
+    const pdfDocGenerator = pdfMake.createPdf(pdfDefinitio);
+    pdfDocGenerator.open();
+  }
+
+  envioCorreo() {
+    Loading.standard('Enviando correo...');
+
+    const data = this.trip;
+    const dataUser = JSON.parse(localStorage.getItem('userData')!);
+    const card = this.cardNumber;
+
+    let params = {
+      data,
+      dataUser,
+      card,
+    };
+
+    console.log(params);
+
+    if (!params) {
+      Notify.init({ position: 'right-bottom' });
+      Loading.remove();
+      Notify.failure(
+        'Error al enviar el correo, complete todos los campos correctamente'
+      );
+
+      return;
+    } else {
+      this.http
+        .post('https://no-country-back-mail.vercel.app/enviarPdf', params)
+        .subscribe((resp: any) => {
+          Loading.remove();
+          Notify.init({ position: 'right-bottom' });
+          console.log(resp);
+          if (params) {
+            Notify.success('Correo enviado correctamente');
+          }
+        });
+    }
+  }
 }
